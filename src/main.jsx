732e2cf8d8dba516e27f30ffaa8bb8b9e307e5c7
arcierim <arcierim@uninorte.edu.co>
2022-11-17 11:54:20 -0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Components/home/Home";
+import Home from "./components/home/Home";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import inputReducer from "./Components/redux/inputs";
